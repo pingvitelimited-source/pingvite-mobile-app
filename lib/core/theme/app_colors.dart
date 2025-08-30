efffe0f04fd5df519dcfa7eb1a642a7a668d9fee
Abhishek Sharma <sharmaabhi4433@gmail.com>
2025-08-31 00:45:39 +0530

    Initial setup and Splash screen

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.deepPurple;
+  static const Color accent = Colors.amber;
+  static const Color background = Colors.white;
+  static const Color darkBackground = Colors.black;
+}
