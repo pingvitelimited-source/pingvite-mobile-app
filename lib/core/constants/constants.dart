23cdb8222df81095493161b5af4d75b1e6b48ce3
Abhishek Sharma <sharmaabhi4433@gmail.com>
2025-08-31 17:33:10 +0530

    new login changes

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,16 @@
 class Constants {
   static const appTitle = 'Pingvite';
+  //Login Screen
+  static const loginHeaderText = "Let’s create deals and offers";
+  static const loginHeaderSubText = "Login to post your offers and deals.";
+  static const loginButton = "Login to continue";
+  static const forgotPasswordText = "Forgot Password?";
+  static const createNewButton = "Create new account";
+  static const madeInIndia = "Made In India";
+
+  //Image URL from Assets
+  static const appLogo = 'assets/png/splashscreen.png';
+  static const email = 'assets/svg/email.svg';
+  static const password = 'assets/svg/password.svg';
+  static const indianFlag = 'assets/svg/india_flag.svg';
 }
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pingvite/core/theme/app_buttom_theme.dart';
+import 'package:pingvite/core/theme/app_image_theme.dart';
+import 'package:pingvite/core/theme/app_text_theme.dart';
 import 'app_colors.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    primaryColor: AppColors.primary,
-    colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
-    appBarTheme: const AppBarTheme(
-      backgroundColor: AppColors.primary,
-      foregroundColor: Colors.white,
-    ),
+    scaffoldBackgroundColor: AppColors.lightbackground,
+    textTheme: GoogleFonts.libreFranklinTextTheme(),
+    extensions: <ThemeExtension<dynamic>>[
+      AppTextTheme.light,
+      AppImageTheme.light,
+      AppButtonTheme.light,
+    ],
   );
 
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    primaryColor: AppColors.primary,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: AppColors.primary,
-      brightness: Brightness.dark,
-    ),
-    appBarTheme: const AppBarTheme(
-      backgroundColor: AppColors.primary,
-      foregroundColor: Colors.white,
-    ),
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    textTheme: GoogleFonts.libreFranklinTextTheme(),
+    extensions: <ThemeExtension<dynamic>>[
+      AppTextTheme.dark,
+      AppImageTheme.dark,
+      AppButtonTheme.dark,
+    ],
   );
 }
